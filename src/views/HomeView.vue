803c4d927f81587932eac156372f05ba6e000a5e
ftaskoski123 <filip.taskoski69@gmail.com>
2024-11-08 17:59:03 +0100

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="$router.push({ name: 'about' })">Go to About</button>
+
+</template>
+
+
+<script setup lang="ts">
+import axios from 'axios';
+import {ref , onMounted} from 'vue'
+
+
+onMounted(()=>{
+
+  axios.get('https://hn.algolia.com/api/v1/search?query=vue')
+  .then(response => {
+
+    console.log(response.data)
+  })
+
+})
+
+</script>
