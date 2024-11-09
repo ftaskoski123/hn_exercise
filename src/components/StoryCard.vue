5f99f472f1bce017e1d6d28acccd266772376bc3
ftaskoski123 <filip.taskoski69@gmail.com>
2024-11-10 00:42:04 +0100

    star icon fix

--- src/components/StoryCard.vue
+++ src/components/StoryCard.vue
@@ -4,7 +4,23 @@
       <div class="story-header">
         <h2 class="story-title">{{ props.story.title }}</h2>
         <div class="icons-wrapper">
-          <img :src="starIcon" @click.stop="toggleFavorite(story)" class="star-icon" :class="{ 'star-icon-active': props.story.isFavorite }" />
+          <svg
+            @click.stop="toggleFavorite(props.story)"
+              class="star-icon"
+              :class="{ 'star-icon-active': props.story.isFavorite }"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              height="37"
+              
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+              />
+            </svg>
           <div class="comment-icon-wrapper">
             <img :src="commentIcon" class="comment-icon" />
             <span class="comment-number">{{ props.story.num_comments }}</span>
@@ -18,7 +34,6 @@
 </template>
 
 <script setup lang="ts">
-import starIcon from '../svgs/star.svg'
 import commentIcon from '../svgs/comment.svg'
 
 const props = defineProps({
@@ -96,23 +111,30 @@ const toggleFavorite = (story: any) : void => {
   gap: 1rem;
 }
 
-.star-icon, .comment-icon {
+.comment-icon {
   width: 1.5rem;
   height: 1.5rem;
   transition: background-color 0.3s ease;
   cursor: pointer;
 }
 
+.star-icon {
+  transition: background-color 0.3s ease;
+  cursor: pointer;
+  margin-left: 1rem;
+  margin-bottom: 0.5rem;
+}
+
 .star-icon:hover {
-  background-color: yellow;
+  fill: yellow;
 }
 
 .star-icon-active {
-  background-color: yellow ; 
+  fill: yellow;
 }
 
 .star-icon-active:hover {
-  background-color: transparent;
+  fill: transparent;
 }
 
 .comment-icon-wrapper {
